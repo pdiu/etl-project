c1593622809c85cf7cec14189fbddc9edea1a0ed
pdiu <phillipliuwr@gmail.com>
2024-09-09 21:55:47 +1200

    trying lowercase sql with uppercase keywords... not sure if i like it but im questioning why ive used all uppercase all this time, its so much harder to read lol

--- models/staging/alphavantage/stg_alphavantage_news__sentiments.sql
+++ models/staging/alphavantage/stg_alphavantage_news__sentiments.sql
@@ -4,58 +4,58 @@
     )
 }}
 
-with stg_news_sentiment as (
-    select
+WITH stg_news_sentiment AS (
+    SELECT
         title
         , url
-        , to_timestamp(time_published, 'YYYYMMDD"T"HH24MISS')::date as publish_date
-        , to_timestamp(time_published, 'YYYYMMDD"T"HH24MISS')::time as publish_time
-        , to_array(parse_json(authors)) as author
+        , to_timestamp(time_published, 'YYYYMMDD"T"HH24MISS')::date AS publish_date
+        , to_timestamp(time_published, 'YYYYMMDD"T"HH24MISS')::time AS publish_time
+        , to_array(parse_json(authors)) AS author
         , summary
         , source
-        , case
-            when category_within_source = 'n/a' then null
-            else category_within_source
-        end as category_within_source
+        , CASE
+            WHEN category_within_source = 'n/a' THEN NULL
+            ELSE category_within_source
+        END AS category_within_source
         , source_domain
         , topics
-        , cast(overall_sentiment_score as decimal(18,8)) as overall_sentiment_score
+        , cast(overall_sentiment_score AS decimal(18,8)) AS overall_sentiment_score
         , overall_sentiment_label
         , ticker_sentiment
 
         -- Metadata
-        , current_user() as insert_user
-        , insert_timestamp::datetime::date as insert_date
-        , insert_timestamp::datetime::time as insert_time
-    from
+        , current_user() AS insert_user
+        , insert_timestamp::datetime::date AS insert_date
+        , insert_timestamp::datetime::time AS insert_time
+    FROM
         {{ source('alphavantage', 'news_sentiment') }}
 )
 
-, add_primary_key as (
-    select
-        md5(to_json(object_construct(* exclude(insert_time)))) as primary_key
+, add_primary_key AS (
+    SELECT
+        md5(to_json(object_construct(* exclude(insert_time)))) AS primary_key
         , *
-    from
+    FROM
         stg_news_sentiment
 )
 
-, remove_duplicates as (
-    select
+, remove_duplicates AS (
+    SELECT
         *
-    from
+    FROM
         add_primary_key
-    qualify
-        row_number() over (
-            partition by primary_key
-            order by insert_date desc
+    QUALIFY
+        ROW_NUMBER() OVER (
+            PARTITION BY PRIMARY_KEY
+            ORDER BY INSERT_DATE DESC
         ) = 1
 )
 
-, final as (
-    select
+, final AS (
+    SELECT
         *
-    from
+    FROM
         remove_duplicates
 )
 
-select * from final
+SELECT * FROM final
